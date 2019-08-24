e925ce9292909cbc63a09a0d401fab9677cf5446
gaoyufei <gaoyufei@zhuozhuo.io>
2019-08-24 14:26:08 +0800

    update

--- ios/Classes/TobiasPlugin.h
+++ ios/Classes/TobiasPlugin.h
@@ -1,1 +1,5 @@
 #import <Flutter/Flutter.h>
+
+@interface TobiasPlugin : NSObject<FlutterPlugin>
++(BOOL)handleOpenURL:(NSURL*)url;
+@end
--- ios/Classes/TobiasPlugin.m
+++ ios/Classes/TobiasPlugin.m
@@ -1,1 +1,14 @@
 #import "TobiasPlugin.h"
+
+
+@interface TobiasPlugin()
+@end
+
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+
+}
+
+
++(BOOL)handleOpenURL:(NSURL*)url{
+  return NO;
+}
